e06c22d337143307933fffc17892c47b217703f4
Weijing Jay Lin <weijingjaylin@gmail.com>
2025-03-17 18:59:52 -0700

    add tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Solutions from './components/Solutions';
-import Clients from './components/Clients';
-import Products from './components/Products';
-import News from './components/News';
-import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Solutions from "./components/Solutions";
+import Clients from "./components/Clients";
+import Products from "./components/Products";
+import News from "./components/News";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
+      <Analytics />
       <Header />
       <Hero />
       <Solutions />
